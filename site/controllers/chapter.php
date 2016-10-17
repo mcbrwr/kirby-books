48aa87039efd659261dcee71d1a1d340bffb6d40
M.C. Brouwer <matthijs@artandflywork.org>
2016-10-17 21:59:42 +0200

    nav buttons for chapters

--- site/controllers/chapter.php
+++ site/controllers/chapter.php
@@ -2,15 +2,46 @@
 
 return function($site, $pages, $page) {
 
-	$rootPage = $page->parents()->last();
-	$book = $rootPage;
+	$book = $pages->findOpen();
 
-	if ($page->depth() < 2 || $rootPage->template() != 'book') {
+	if ($page->depth() < 2 || $book->template() != 'book') {
 		$indexTitle = $site->title();
 	} else {
-		$indexTitle = $rootPage->title();
+		$indexTitle = $book->title();
 	}
 
-  return compact('rootPage','book','indexTitle');
+	// dump($pages->findOpen()->index()->first()->title());
+	// die();
+	// $test = $pages->findOpen()->index()->findBy('uri',$page->uri())->next()->title();
+
+	// $test = $page->uri();
+
+	// build next page var
+	if ($page->hasChildren()) {
+		$next = $page->children()->first();
+	} else if ($page->hasNext()) {
+		$next = $page->next();
+	} else if ($page->depth() > 2 && $page->parent()->hasNext()) {
+		$next = $page->parent()->next();
+	} else {
+		$next = false;
+	}
+
+	// build prev page var
+	if ($page->hasPrev() && $page->prev()->hasChildren()) {
+		$prev = $page->prev()->children()->last();
+	} else if ($page->hasPrev()) {
+		$prev = $page->prev();
+	} else if ($page->hasParent() && $page->depth() > 2) {
+		$prev = $page->parent();
+	} else {
+		$prev = false;
+	}
+
+
+
+	
+
+  return compact('next','prev','book','indexTitle');
 
 };
--- site/templates/chapter.php
+++ site/templates/chapter.php
@@ -3,5 +3,22 @@
 	<h1><?php echo $page->title()->html() ?></h1>
 
 	<?php echo $page->body()->kirbytext(); ?>
+	
+	<footer class="chapter-footer">
+		<nav class="chapter-nav">
+			<?php if ($prev): ?>
+				<a href="<?php echo $prev->url() ?>">
+					<svg width="32px" height="28px" viewBox="0 0 32 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-412.000000, -933.000000)" fill="#000000"> <path d="M438,957 C438,958.104 437.104,959 436,959 L416,959 C414.896,959 414,958.104 414,957 L414,937 C414,935.896 414.896,935 416,935 L436,935 C437.104,935 438,935.896 438,937 L438,940 L440,940 L440,937 C440,934.791 438.209,933 436,933 L416,933 C413.791,933 412,934.791 412,937 L412,957 C412,959.209 413.791,961 416,961 L436,961 C438.209,961 440,959.209 440,957 L440,954 L438,954 L438,957 L438,957 Z M443,946 L421.414,946 L426.657,940.757 C427.048,940.367 427.048,939.733 426.657,939.343 C426.267,938.952 425.633,938.952 425.242,939.343 L418.343,946.242 C418.135,946.451 418.046,946.728 418.06,947 C418.046,947.272 418.135,947.549 418.343,947.758 L425.242,954.657 C425.633,955.048 426.267,955.048 426.657,954.657 C427.048,954.267 427.048,953.633 426.657,953.242 L421.414,948 L443,948 C443.553,948 444,947.553 444,947 C444,946.448 443.553,946 443,946 L443,946 Z" id="arrow-left" sketch:type="MSShapeGroup"></path> </g> </g> </svg>
+					<span class="sr-only">Previous Chapter</span>
+				</a>
+			<?php endif ?>
+			<?php if ($next): ?>
+				<a href="<?php echo $next->url() ?>">
+					<svg width="32px" height="28px" viewBox="0 0 32 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-360.000000, -933.000000)" fill="#000000"> <path d="M388,933 L368,933 C365.791,933 364,934.791 364,937 L364,941 L366,941 L366,937 C366,935.896 366.896,935 368,935 L388,935 C389.104,935 390,935.896 390,937 L390,957 C390,958.104 389.104,959 388,959 L368,959 C366.896,959 366,958.104 366,957 L366,953 L364,953 L364,957 C364,959.209 365.791,961 368,961 L388,961 C390.209,961 392,959.209 392,957 L392,937 C392,934.791 390.209,933 388,933 L388,933 Z M377.343,953.243 C376.953,953.633 376.953,954.267 377.343,954.657 C377.733,955.048 378.367,955.048 378.758,954.657 L385.657,947.758 C385.865,947.549 385.954,947.272 385.94,947 C385.954,946.728 385.865,946.451 385.657,946.243 L378.758,939.344 C378.367,938.953 377.733,938.953 377.343,939.344 C376.953,939.733 376.953,940.367 377.343,940.758 L382.586,946 L361,946 C360.447,946 360,946.448 360,947 C360,947.553 360.447,948 361,948 L382.586,948 L377.343,953.243 L377.343,953.243 Z" id="arrow-right" sketch:type="MSShapeGroup"></path> </g> </g> </svg>
+					<span class="sr-only">Next Chapter</span>
+				</a>
+			<?php endif ?>
+		</nav>
+	</footer>
 
 <?php snippet('footer') ?>
